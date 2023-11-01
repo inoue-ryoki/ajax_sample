bf3c5619abe0b0450465d3e6049f54095d98b1a2
inoue-ryoki <mth.1021@icloud.com>
2023-11-01 21:55:43 +0900

    これで完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :messages do
+    resources :searches, only: :index, defaults: { format: :json }
+  end
   resources :messages
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
